a31e4f72091d0e99191d39afdcddd60038940acb
Ammandour8 <amrmandour51@gmail.com>
2024-05-22 14:35:07 -0400

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python3
-""" Script to get TODO list progress
-    by employee ID and save it to
-    CSV file
-"""
-from csv import writer, QUOTE_ALL
-from requests import get
-from sys import argv
+"""Accessing a REST API for todo lists of employees"""
 
-
-def todo_csv(emp_id):
-    """ Send request for employee's
-        to do list to API
-    """
-    file_name = '{}.csv'.format(emp_id)
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
-
-    # check if user exists
-    user = get(url_user + emp_id).json().get('username')
-
-    if user:
-        params = {'userId': emp_id}
-        #  get all tasks
-        tasks = get(url_todo, params=params).json()
-        if tasks:
-            #  open file in write mode and use csv writer to
-            #  writer content
-            with open(file_name, 'w', newline='', encoding='utf8') as f:
-                task_writer = writer(f, quoting=QUOTE_ALL)
-                for task in tasks:
-                    task_writer.writerow([emp_id, user, task.get('completed'),
-                                         task.get('title')])
+import requests
+import sys
 
 
 if __name__ == '__main__':
-    if len(argv) > 1:
-        todo_csv(argv[1])
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
